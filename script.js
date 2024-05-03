1be8d415ec9e6e529e0ca8d2c40de31410b29be1
musanurbadr <111696681+musanurbadr@users.noreply.github.com>
2024-05-03 21:51:09 +0300

    Interactive rating component finished

--- script.js
+++ script.js
@@ -7,35 +7,56 @@ let message = document.getElementById("selectedRating");
 
 let baack = document.getElementById("back");
 
-icon.forEach(element => {
-    element.addEventListener('click', () => {
-        showInput(element);
+let front = document.getElementById("front");
 
-    });
-    btn.addEventListener('click', function (){
-        
+let box = null;
+
+icon.forEach((reting_icon)=>{
+    reting_icon.addEventListener("click" , (e)=>{
+        const active = document.querySelector('.checked');
+        if(active){
+            active.classList.remove("checked")
+        }
+        const card = e.target;
+        card.classList.add("checked");
+        rate = e.target.innerText;
+
     })
+})
+btn.addEventListener("click" , () => {
+    if(rate){
+        message.innerText = rate;
+        front.classList.add("hidden");
+        baack.classList.remove("hidden");
+    }
 });
-function showInput (select){
-    select.classList.add('baack');
-}
 
 
 
+// icon.forEach(element => {
+//     element.addEventListener('click', () => {
+//         showInput(element);
+
+//     });
+
+//     btn.addEventListener('click', function (){
+        
+//     })
+// });
+// function ewd (select){
+//     select.classList.add('baack');
+// }
 
 
-
-function showInput(element){
-    let value = parseInt(element.getAttribute('data-value'));
-    let orangeColor = getComputedStyle(document.body).getPropertyValue('--Orange');
-    element.style.backgroundColor = orangeColor;
-    element.style.color = "White";
-    message.innerText = value;
-    btn.click(
-    ); // Submit button'a otomatik tıklama yapar
-}
-
-
+// function showInput(element){
+//     let value = parseInt(element.getAttribute('data-value'));
+//     let orangeColor = getComputedStyle(document.body).getPropertyValue('--Orange');
+//     element.style.backgroundColor = orangeColor;
+//     element.style.color = "White";
+//     message.innerText = value;
+//     btn.click(
+//     ); // Submit button'a otomatik tıklama yapar
+// }
 
 
 
